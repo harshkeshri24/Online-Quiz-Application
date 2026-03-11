4e2782eb36b9043d4fe29085f6c537ed9786fd25
harshkeshri24 <harshkeshri65@gmail.com>
2026-03-12 00:21:34 +0530

    Add Question class for quiz functionality

---
+++ Question.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class Question {
+    private final String questionText;
+    private final String[] options;
+    private final int correctAnswerIndex;
+
+    public Question(String questionText, String[] options, int correctAnswerIndex) {
+        this.questionText = questionText;
+        this.options = options;
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+
+    public String getQuestionText() { return questionText; }
+    public String[] getOptions() { return options; }
+    public int getCorrectAnswerIndex() { return correctAnswerIndex; }
+}
